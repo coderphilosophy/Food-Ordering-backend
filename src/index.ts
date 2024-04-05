d7397ca87e932fd48b5253295b8dd979581d7799
Parshva Gang <coderphilosophy09@gmail.com>
2024-04-05 14:46:05 +0530

    Add create restaurant form page

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
 import MyUserRoute from "./routes/MyUserRoute"
+import { v2 as cloudinary } from "cloudinary"
+import MyRestaurantRoute from "./routes/MyRestaurantRoute"
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log("Connected to database"))
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express()
 
@@ -18,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 })
 
 app.use("/api/my/user", MyUserRoute)
+app.use("/api/my/restaurant", MyRestaurantRoute)
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")

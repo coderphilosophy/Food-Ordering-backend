bf75b02d5132b6cc421033077897b40eed11bb68
Parshva Gang <coderphilosophy09@gmail.com>
2024-04-21 16:42:41 +0530

    Add payment interface using Stripe, set up StripeCLI

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute"
+import orderRoute from "./routes/OrderRoute"
 import { v2 as cloudinary } from "cloudinary"
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
 import restaurantRoute from "./routes/RestaurantRoutes"
@@ -29,6 +30,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute)
 app.use("/api/my/restaurant", myRestaurantRoute)
 app.use("/api/restaurant", restaurantRoute)
+app.use("/api/order", orderRoute)
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")
